ac8a70c2feb3c26a1ec85a3641512fced6273d0f
Will Beaumont <39424606+nmbeau@users.noreply.github.com>
2022-09-28 22:37:19 -0700

    clean up create-react-app boiler plate

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+
+function DrumPad(props) {
+
+  return (
+    <button
+      className="drum-pad bg-teal-500 hover:bg-teal-600 w-16 h-16 border border-sky-900 rounded-md"
+      id={props.drumKey.description}
+      type="button"
+      onClick={playKey}>
+      {props.drumKey.name.toUpperCase()}
+      <audio
+        ref={ref}
+        className="clip"
+        id={props.drumKey.name.toUpperCase()}
+        src={props.drumKey.audio}
+        desc={props.drumKey.description}
+      />
+    </button>
+   );
+}
+
 
 function App() {
   return (
